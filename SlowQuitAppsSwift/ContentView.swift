40753499c537bd6492ba4c70870314298ed12495
yaotutu <957810668@qq.com>
2025-11-28 19:47:40 +0800

    Initial Commit

---
+++ SlowQuitAppsSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlowQuitAppsSwift
+//
+//  Created by yaotutu on 2025/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlowQuitAppsSwift/SlowQuitAppsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowQuitAppsSwiftApp.swift
+//  SlowQuitAppsSwift
+//
+//  Created by yaotutu on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct SlowQuitAppsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
